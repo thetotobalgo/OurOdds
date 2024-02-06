4ee32e7234c5d608d9bfa6d2c06ea235f9ef6efb
thetotobalgo <leboudeur75@gmail.com>
2024-02-06 10:11:11 +0100

    fix: import typo

--- app/components/layout.js
+++ app/components/layout.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Header from '../components/Header';
+import Header from '../components/header';
 import Footer from '../components/footer'; // Ensure the import path matches the case of your filename
 
 export default function Layout({ children, title = 'Digital Publishing Platform', description = 'A minimal digital publishing platform' }) {
